435cfc70df4621661853a5db9bcab50755ea48df
Tuan Nguyen <tuan.beplus@gmail.com>
2025-10-10 14:07:58 +0700

    Update plg ver

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Assessment
 Plugin URI: #
 Description: Custom plugin for Project Saturn
-Version: 2.9.9
+Version: 3.0.0
 Author: tom@ysnstudios.com
 Author URI: tom@ysnstudios.com
 Text Domain: wp-assessment
